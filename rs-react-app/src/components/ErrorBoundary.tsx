476e477169efdac68e673550e4495c0a141d2f95
Meru <173436493+merucoding@users.noreply.github.com>
2026-05-04 17:53:09 +0500

    feat: add Go back button

--- rs-react-app/src/components/ErrorBoundary.tsx
+++ rs-react-app/src/components/ErrorBoundary.tsx
@@ -1,4 +1,5 @@
 import { Component, type ErrorInfo, type ReactNode } from 'react';
+import { BORDER_STYLE, BUTTON_HOVER_STYLE } from '../types/constants';
 
 type ErrorBoundaryProps = { children: ReactNode };
 type ErrorBoundaryState = { hasError: boolean };
@@ -14,13 +15,23 @@ export default class ErrorBoundary extends Component<ErrorBoundaryProps> {
     console.error('Caught an error:', error, info.componentStack);
   }
 
+  handleReload = () => {
+    this.setState({ hasError: false });
+  };
+
   render() {
     if (this.state.hasError) {
       return (
-        <div className="flex justify-center items-center h-100">
+        <div className="flex flex-col gap-y-6 justify-center items-center h-100">
           <h1 className="font-lexend-exa text-fuchsia-400 font-bold text-2xl">
-            Something went wrong..
+            Something went wrong...
           </h1>
+          <button
+            onClick={this.handleReload}
+            className={`${BORDER_STYLE} ${BUTTON_HOVER_STYLE} font-lexend-exa text-emerald-500 font-light`}
+          >
+            Go back
+          </button>
         </div>
       );
     }
