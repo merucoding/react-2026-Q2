3606d7376347dafc95b2a6057f1d80323bc7d215
Meru <173436493+merucoding@users.noreply.github.com>
2026-05-11 11:28:47 +0500

    test: add Loading component test

---
+++ rs-react-app/src/components/Loading/index.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import Loading from './';
+import { describe, it, expect } from 'vitest';
+
+describe('Loading component', () => {
+  it('renders spinner', () => {
+    render(<Loading />);
+
+    expect(screen.getByTestId('spinner')).toBeInTheDocument();
+  });
+});
