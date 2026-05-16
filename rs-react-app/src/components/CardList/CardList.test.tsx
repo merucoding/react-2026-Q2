a5a3061c8ffbf1305ca762c17ecb95eb4d9ec8e4
Meru <173436493+merucoding@users.noreply.github.com>
2026-05-17 00:34:19 +0500

    test: fix CardList component test

--- rs-react-app/src/components/CardList/CardList.test.tsx
+++ rs-react-app/src/components/CardList/CardList.test.tsx
@@ -2,10 +2,15 @@ import { render, screen } from '@testing-library/react';
 import CardList from './CardList';
 import { MOCK_POKEMONS_DATA } from '../../test-utils/mocks/handlers/pokemonMocks';
 import type { PokemonType } from '../../api/fetchPokemons';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('CardList component', () => {
   it('renders correct number of pokemons when data is provided', () => {
-    render(<CardList pokemons={MOCK_POKEMONS_DATA as PokemonType[]} />);
+    render(
+      <MemoryRouter>
+        <CardList pokemons={MOCK_POKEMONS_DATA as PokemonType[]} />
+      </MemoryRouter>
+    );
 
     expect(screen.getAllByRole('img')).toHaveLength(MOCK_POKEMONS_DATA.length);
     expect(screen.getByText('bulbasaur')).toBeInTheDocument();
