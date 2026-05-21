d91778a2e46c93bf9e200afc29a81c3fd18aa613
Meru <173436493+merucoding@users.noreply.github.com>
2026-05-21 14:21:57 +0500

    chore: install redux toolkit, configure store

--- rs-react-app/src/main.tsx
+++ rs-react-app/src/main.tsx
@@ -2,14 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
 
 createRoot(rootElement).render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </StrictMode>
 );
